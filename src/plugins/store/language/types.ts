a9390ad50845cffbb94e6194a7fac91cd93b1bff
Suprianto Djamalu <erthru@Supriantos-MacBook-Pro.local>
2020-12-13 17:49:19 +0800

    prepare lang

---
+++ src/plugins/store/language/types.ts
@@ -0,0 +1,23 @@
+export enum LANGUAGE_TYPES {}
+
+export const LANGUAGE_COL_NAME = "languages";
+
+export enum LanguageField {
+    lang = "lang",
+    level = "level",
+    createdOn = "createdOn",
+    updatedOn = "updatedOn",
+}
+
+export enum LanguageLevel {
+    active = "active",
+    passive = "passive",
+}
+
+export type Language = {
+    id?: string;
+    [LanguageField.lang]?: string;
+    [LanguageField.level]?: LanguageLevel;
+    [LanguageField.createdOn]?: Date;
+    [LanguageField.updatedOn]?: Date;
+};

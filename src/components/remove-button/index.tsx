f402a6076c4c2f04e9274f2be803d3d9aea9af14
Suprianto Djamalu <erthru@Supriantos-MacBook-Pro.local>
2020-12-17 02:30:02 +0800

    add confirmation on delete

--- src/components/remove-button/index.tsx
+++ src/components/remove-button/index.tsx
@@ -1,7 +1,9 @@
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Button from "../button";
+import Modal from "../modal";
 
 type Props = {
     className?: string;
@@ -9,15 +11,38 @@ type Props = {
     onClick?(): void;
 };
 
-const RemoveButton = (props: Props) => (
-    <Link
-        to={props.to === undefined ? "#" : props.to}
-        className={"flex text-red-600 items-center font-medium cursor-pointer " + props.className}
-        onClick={props.onClick}
-    >
-        <FontAwesomeIcon icon={faTrashAlt} />
-        <span className="ml-2">Remove</span>
-    </Link>
-);
+const RemoveButton = (props: Props) => {
+    const [isDeleteModalShown, setIsDeleteModalShown] = useState(false);
+
+    return (
+        <div className={"flex " + props.className}>
+            <div className="flex mx-auto text-red-600 items-center font-medium cursor-pointer" onClick={() => setIsDeleteModalShown(true)}>
+                <FontAwesomeIcon icon={faTrashAlt} />
+                <span className="ml-2">Remove</span>
+            </div>
+
+            <Modal title="Delete Confirmation" isShown={isDeleteModalShown}>
+                <span className="w-full flex">Delete this item ?</span>
+
+                <div className="flex mt-3">
+                    <Link
+                        className="cursor-pointer font-medium text-red-600"
+                        to={props.to === undefined ? "#" : props.to}
+                        onClick={() => {
+                            props.onClick!!();
+                            setIsDeleteModalShown(false);
+                        }}
+                    >
+                        Delete
+                    </Link>
+
+                    <span className="text-gray-600 ml-2 cursor-pointer font-medium" onClick={() => setIsDeleteModalShown(false)}>
+                        Cancel
+                    </span>
+                </div>
+            </Modal>
+        </div>
+    );
+};
 
 export default RemoveButton;
